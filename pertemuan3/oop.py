9e8bd593d4af0ad8360c1e38adbf900a2c1e6b57
najwa820 <najwa.khairunnisa1300@student.unri.ac.id>
2026-02-21 21:28:29 +0700

    Tugas Pertemuan 3

--- pertemuan3/oop.py
+++ pertemuan3/oop.py
@@ -1,6 +1,7 @@
 #Penulisan kode berorientasi objek
 #agar punya struktur yang clear, mudah dibaca, dan di debug
 #oop ga lepas dari class (cetak biru) dan objek (hasil dari blue print)
+#di oop juga ada method dan atribut 
 
 #__init__ method
 class Person:
